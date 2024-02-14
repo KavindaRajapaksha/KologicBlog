281390de0ae035c9f2c8100596f5667796a2aa6e
Kavinda <rdks.kavinda@gmail.com>
2024-02-14 12:04:27 +0530

    Create signup controllers

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+  const hashPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "Signup success! Please signin." });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+};
