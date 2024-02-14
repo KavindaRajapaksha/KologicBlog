22bb31b9597d952263ece67426299811fae27b42
Kavinda <rdks.kavinda@gmail.com>
2024-02-14 22:56:49 +0530

    complete sign up interface

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,43 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3  max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-9'>
+        <div className='flex-1'>
+        <Link
+        to="/"
+        className="  font-bold dark:text-white text-4xl"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          KOLOGIC
+        </span>
+        Blog
+      </Link>
+      <p className='text-sm mt-5'>"Welcome to our blog, your go-to destination for insightful articles, helpful tips, and inspiring stories. Join us as we explore diverse topics and share valuable insights with our readers."</p>
+        </div>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'>
+          <div>
+                <Label value='Your username'  />
+                <TextInput type='text' placeholder='Username' id='username' />
+            </div><div>
+                <Label value='Your email'  />
+                <TextInput type='text' placeholder='Email' id='email' />
+            </div><div>
+                <Label value='Your password'  />
+                <TextInput type='text' placeholder='Password' id='password' />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit'>Sign Up</Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Already have an account?</span>
+            <Link to='/sign-in' className='text-purple-500'>Sign In</Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
