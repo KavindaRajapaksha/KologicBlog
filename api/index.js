2c43bfed8f50dae4eff76debe55a014c305ebb72
Kavinda <rdks.kavinda@gmail.com>
2024-02-14 11:31:28 +0530

    Create Routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -14,4 +15,7 @@ const app=express();
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log('Server is running on port '+PORT+'...');
-})
+});
+
+app.use(express.json());
+app.use('/api/user',userRoutes);
