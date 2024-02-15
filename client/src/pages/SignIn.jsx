1851b53a6cc1a3992b05de3b5968da4c97af1014
Kavinda <rdks.kavinda@gmail.com>
2024-02-15 13:24:00 +0530

    Complete Sign In Function

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,97 @@
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function SignIn() {
+  const[formData,setFormData] = useState({})
+  const [errorMessage,setErrorMessage] = useState(null)
+  const [loading,setLoading] = useState(false)
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData({...formData,[e.target.id]:e.target.value.trim()})
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if( !formData.email || !formData.password){
+      setErrorMessage('Please fill in all fields')
+      return
+    }
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+       const res= await fetch('/api/auth/signin',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(formData)
+       }
+       
+       );
+        const data = await res.json()
+        if(data.success===false){
+          setErrorMessage(data.message)
+          return
+        }
+        setLoading(false);
+        navigate('/')
+
+    }catch(error){
+       setErrorMessage('An error occured, please try again')
+        setLoading(false);
+    }
+
+  }
+
+
+
   return (
-    <div>SignIn</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3  max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-9'>
+        <div className='flex-1'>
+        <Link
+        to="/"
+        className="  font-bold dark:text-white text-4xl"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          KOLOGIC
+        </span>
+        Blog
+      </Link>
+      <p className='text-sm mt-5'>"Welcome to our blog, your go-to destination for insightful articles, helpful tips, and inspiring stories. Join us as we explore diverse topics and share valuable insights with our readers."</p>
+        </div>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+          <div>
+                <Label value='Your email'  />
+                <TextInput type='email' placeholder='Email' id='email' onChange={handleChange}/>
+            </div><div>
+                <Label value='Your password'  />
+                <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>{
+              loading ?
+              <>
+              <Spinner  size='sm' />
+              <span className='pl-3'>Loading...</span>
+              </>
+              : 'Sign In'
+            }</Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Don't have an account?</span>
+            <Link to='/sign-up' className='text-purple-500'>Sign Up</Link>
+          </div>
+          {
+            errorMessage &&(
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
+    </div>
   )
 }
