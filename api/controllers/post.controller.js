e38b136c5f5ee381f8b75d725bbd082bc9743e85
Kavinda <rdks.kavinda@gmail.com>
2024-02-24 15:26:02 +0530

    Create getPosts Api Route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,26 +1,72 @@
-import { errorHandler } from "../utills/error.js"
-import Post from "../models/postModel.js"
+import { errorHandler } from "../utills/error.js";
+import Post from "../models/postModel.js";
 
-export const create = async (req, res,next) => {
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all require fields"));
+  }
+  const slug = req.body.title
+    .toLowerCase()
+    .split(" ")
+    .join("-")
+    .replace(/[^a-zA-Z0-9-]/g, "-");
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savePost = await newPost.save();
+    res.status(201).json(savePost);
+  } catch (err) {
+    next(err);
+  }
+};
+export const getpost = async (req, res, next) => {
+    try {
+      const startIndex = parseInt(req.query.startIndex) || 0;
+      const limit = parseInt(req.query.limit) || 9;
+      const sortDirection = req.query.order === 'asc' ? 1 : -1;
+      const posts = await Post.find({
+        ...(req.query.userId && { userId: req.query.userId }),
+        ...(req.query.category && { category: req.query.category }),
+        ...(req.query.slug && { slug: req.query.slug }),
+        ...(req.query.postId && { _id: req.query.postId }),
+        ...(req.query.searchTerm && {
+          $or: [
+            { title: { $regex: req.query.searchTerm, $options: 'i' } },
+            { content: { $regex: req.query.searchTerm, $options: 'i' } },
+          ],
+        }),
+      })
+        .sort({ updatedAt: sortDirection })
+        .skip(startIndex)
+        .limit(limit);
   
-    if(!req.user.isAdmin){
-        return  next(errorHandler(403,'You are not allowed to create a post'))
-    }
-    if(!req.body.title || !req.body.content){
-        return next(errorHandler(400,'Please provide all require fields'))
-    }
-    const slug = req.body.title.toLowerCase().split(' ').join('-').replace(/[^a-zA-Z0-9-]/g, "-");
-    const newPost = new Post({
-        ...req.body,
-        slug,
-        userId: req.user.id,
+      const totalPosts = await Post.countDocuments();
+  
+      const now = new Date();
+  
+      const oneMonthAgo = new Date(
+        now.getFullYear(),
+        now.getMonth() - 1,
+        now.getDate()
+      );
+  
+      const lastMonthPosts = await Post.countDocuments({
+        createdAt: { $gte: oneMonthAgo },
       });
-    try{
-        const savePost=await newPost.save()
-        res.status(201).json(savePost)
-    }catch(err){
-        next(err)
-    }
   
-
-}
+      res.status(200).json({
+        posts,
+        totalPosts,
+        lastMonthPosts,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+  
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utills/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
+import { create,getpost } from '../controllers/post.controller.js';
 const router = express.Router();
 
 router.post('/create',verifyToken,create)
+router.get('/getposts',getpost)
 
 
 
