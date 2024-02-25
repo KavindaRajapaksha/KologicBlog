0310f7e0fd96492309d5957e8fcef95648ca3168
Kavinda <rdks.kavinda@gmail.com>
2024-02-25 15:26:13 +0530

    Create update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utills/verifyUser.js';
-import { create,getpost,deletepost } from '../controllers/post.controller.js';
+import { create,getpost,deletepost,updatepost } from '../controllers/post.controller.js';
 const router = express.Router();
 
 router.post('/create',verifyToken,create)
 router.get('/getposts',getpost)
 router.delete('/deletepost/:postId/:userId',verifyToken,deletepost)
+router.put('/updatepost/:postId/:userId',verifyToken,updatepost)
 
 
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 
 export default function App() {
@@ -28,6 +29,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         <Route element={<OnlyAdminPrivateRoute />}> 
         <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
       </Routes>
        
