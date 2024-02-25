c63bfa7f4cb4511d1bf7dc89205c5c6a527f1104
Kavinda <rdks.kavinda@gmail.com>
2024-02-25 23:06:10 +0530

    Create get user Route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import {test} from '../controllers/user.controller.js';
-import {updateUser,deleteUser,signout} from '../controllers/user.controller.js';
+import {updateUser,deleteUser,signout,getUsers} from '../controllers/user.controller.js';
 import { verifyToken } from '../utills/verifyUser.js';
+
 
 
 const router=express.Router();
@@ -11,5 +12,6 @@ router.get('/test',test);
 router.put('/update/:userId',verifyToken,updateUser);
 router.delete('/delete/:userId',verifyToken,deleteUser);
 router.post('/signout',signout);
+router.get('/getusers',verifyToken,getUsers)
 
 export default router;
