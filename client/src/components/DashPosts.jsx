0c27d7d29410e01ac92aa84a02c5587b095f85fa
Kavinda <rdks.kavinda@gmail.com>
2024-02-25 13:23:50 +0530

    Add showMore Functionality to the Dash posts

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import {Table} from 'flowbite-react';
 import { Link } from 'react-router-dom';
+import { data } from 'autoprefixer';
 
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -26,7 +28,26 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+  const handleShowMore = async () => {
+     const startIndex = userPosts.length;
+     try{
+      const res=await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data  = await res.json();
+      if(res.ok){
+       setUserPosts((prev) => [...prev, ...data.posts]);
+       if (data.posts.length < 9) {
+         setShowMore(false);
+       }
+      }
+     }catch(error){
+       console.log(error.message);
+     }
+  
+  
+  
+  }
    return(
+
 
     <div className=' table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
     {
@@ -81,6 +102,14 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
         </>
       ) :(
         <p> No posts to display </p>
