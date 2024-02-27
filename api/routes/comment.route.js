8daf5a6b619ed92878c31e8e853ef25490be77ca
Kavinda <rdks.kavinda@gmail.com>
2024-02-27 21:08:40 +0530

    comment delete function

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment,getPostComments,likeComment,editComment } from '../controllers/comment.controller.js';
+import { createComment,getPostComments,likeComment,editComment,deleteComment } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utills/verifyUser.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/create',verifyToken,createComment);
 router.get('/getPostComments/:postId',getPostComments);
 router.put('/likeComment/:commentId',verifyToken,likeComment);
 router.put('/editComment/:commentId',verifyToken,editComment);  
+router.delete('/deleteComment/:commentId',verifyToken,deleteComment);
 
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,18 +1,18 @@
-
+import { Alert, Button, Modal, TextInput, Textarea } from 'flowbite-react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { useState,useEffect } from 'react';
-import { Textarea, Button,Alert } from 'flowbite-react';
 import Comment from './Comment';
-
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState('');
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
-
-  
+  const [showModal, setShowModal] = useState(false);
+  const [commentToDelete, setCommentToDelete] = useState(null);
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (comment.length > 200) {
@@ -40,6 +40,7 @@ export default function CommentSection({ postId }) {
       setCommentError(error.message);
     }
   };
+
   useEffect(() => {
     const getComments = async () => {
       try {
@@ -54,7 +55,7 @@ export default function CommentSection({ postId }) {
     };
     getComments();
   }, [postId]);
-  const navigate = useNavigate();
+
   const handleLike = async (commentId) => {
     try {
       if (!currentUser) {
@@ -81,27 +82,34 @@ export default function CommentSection({ postId }) {
     } catch (error) {
       console.log(error.message);
     }
+  };
 
-  }
-  const handleEdit=async(comment,editedContent)=>{
-   setComments(
-    comments.map((c) =>
-      c._id === comment._id
-        ? {
-            ...c,
-            content: editedContent,
-          }
-        : c
-    )
-   )
-  
-  
-  
-  }
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
 
-
-  
- 
+  const handleDelete = async (commentId) => {
+    setShowModal(false);
+    try {
+      if (!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+      const res = await fetch(`/api/comment/deleteComment/${commentId}`, {
+        method: 'DELETE',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(comments.filter((comment) => comment._id !== commentId));
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
@@ -138,11 +146,10 @@ export default function CommentSection({ postId }) {
             maxLength='200'
             onChange={(e) => setComment(e.target.value)}
             value={comment}
-          
           />
           <div className='flex justify-between items-center mt-5'>
             <p className='text-gray-500 text-xs'>
-              {200-comment.length} characters remaining
+              {200 - comment.length} characters remaining
             </p>
             <Button outline gradientDuoTone='purpleToBlue' type='submit'>
               Submit
@@ -153,9 +160,7 @@ export default function CommentSection({ postId }) {
               {commentError}
             </Alert>
           )}
-
         </form>
-       
       )}
       {comments.length === 0 ? (
         <p className='text-sm my-5'>No comments yet!</p>
@@ -173,12 +178,41 @@ export default function CommentSection({ postId }) {
               comment={comment}
               onLike={handleLike}
               onEdit={handleEdit}
-             
+              onDelete={(commentId) => {
+                setShowModal(true);
+                setCommentToDelete(commentId);
+              }}
             />
           ))}
         </>
       )}
-     
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size='md'
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+              Are you sure you want to delete this comment?
+            </h3>
+            <div className='flex justify-center gap-4'>
+              <Button
+                color='failure'
+                onClick={() => handleDelete(commentToDelete)}
+              >
+                Yes, I'm sure
+              </Button>
+              <Button color='gray' onClick={() => setShowModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
