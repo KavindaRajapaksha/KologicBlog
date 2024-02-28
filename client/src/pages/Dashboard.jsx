5c5a3dd51346100ac876f3cdb6dab0989f1014f4
Kavinda <rdks.kavinda@gmail.com>
2024-02-28 22:47:14 +0530

    Styling the final css

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
@@ -10,7 +10,6 @@ import DashboardComp from '../components/DashboardComp';
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
-
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get('tab');
@@ -18,19 +17,16 @@ export default function Dashboard() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
       <div className='md:w-56'>
         <DashSidebar />
       </div>
-      <div>
-        {tab === 'profile' && <DashProfile />}
-        {tab === 'posts' && <DashPosts />}
-        {tab === 'users' && <DashUsers />}
-        {tab==='comments' && <DashComments />}
-        {tab==='dash' && <DashboardComp/>}
-      </div>
+      {tab === 'profile' && <DashProfile />}
+      {tab === 'posts' && <DashPosts />}
+      {tab === 'users' && <DashUsers />}
+      {tab === 'comments' && <DashComments />}
+      {tab === 'dash' && <DashboardComp />}
     </div>
   );
 }
