1d135775260306bbdfb1126844f315f7d75a1873
Kavinda <rdks.kavinda@gmail.com>
2024-02-28 17:00:03 +0530

    complete the functional requirements of the project

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utills/verifyUser.js';
-import { create,getpost,deletepost,updatepost } from '../controllers/post.controller.js';
+import { create,getposts,deletepost,updatepost } from '../controllers/post.controller.js';
 const router = express.Router();
 
 router.post('/create',verifyToken,create)
-router.get('/getposts',getpost)
+router.get('/getposts',getposts)
 router.delete('/deletepost/:postId/:userId',verifyToken,deletepost)
 router.put('/updatepost/:postId/:userId',verifyToken,updatepost)
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,30 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation,useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon,FaSun } from 'react-icons/fa';
 import { useSelector,useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice.js';
 import { signoutSuccess } from '../redux/user/userSlice.js';
+import { useEffect, useState  } from 'react';
 
 export default function Header() {
   const path = useLocation().pathname;
   const { currentUser } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+  const pathName = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
+  
+
+  useEffect(() => {  
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   const handleSignOut= async()=>{
     try{
@@ -28,6 +42,15 @@ export default function Header() {
     }
 
   }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+
+   
+  }
   
 
   return (
@@ -41,12 +64,14 @@ export default function Header() {
         </span>
         Blog
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
           type="text"
           placeholder="Search..."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
