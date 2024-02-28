4d39084a8a59066ec27eb2e507362824080ff29b
Kavinda <rdks.kavinda@gmail.com>
2024-02-28 23:52:13 +0530

    test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
         <h1 className="text-3xl font-bold lg:text-6xl">
-          Welcome to KOLOGIC Blog!
+          Welcome to KOLOGIC Blog!!
         </h1>
         <p className="text-gray-500 text-xs sm:text-sm">
           "Explore a diverse world of knowledge at KOLOGIC Blog, where
